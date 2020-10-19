ad6c765e043ba1b2fe475d70e1706ed75f7ffa96
darshan <darshanaswath@gmail.com>
2020-10-19 19:08:47 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,13 +28,50 @@ class _MyHomePageState extends State<MyHomePage> {
   File pickedImage;
   bool isImageLoaded = false;
 
+  List _results;
+  String _confidence = "";
+  String _names = "";
+
   getFromGallery() async {
     var tempStore = await ImagePicker().getImage(source: ImageSource.gallery);
 
     setState(() {
       pickedImage = File(tempStore.path);
       isImageLoaded = true;
+      applyMyModel(pickedImage);
     });
+  }
+
+  loadMyModel() async {
+    var resultant = await Tflite.loadModel(
+      labels: "assets/labels.txt",
+      model: "assets/model_unquant.tflite",
+    );
+  }
+
+  applyMyModel(File file) async {
+    var res = await Tflite.runModelOnImage(
+      path: file.path,
+      numResults: 2,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _results = res;
+      print(res);
+      String str = _results[0]["label"];
+      _names = str.substring(2);
+      _confidence = _results != null
+          ? (_results[0]['confidence'] * 100.0).toString().substring(0, 4) + "%"
+          : "";
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadMyModel();
   }
 
   @override
@@ -58,7 +96,27 @@ class _MyHomePageState extends State<MyHomePage> {
                       )),
                     ),
                   )
-                : Container()
+                : Container(),
+            Text("Name : $_names \nConfidence : $_confidence"),
+            SizedBox(
+              height: 50,
+            ),
+            FlatButton(
+              color: Colors.indigo,
+              onPressed: () {
+                setState(() {
+                  pickedImage = File("");
+                  _confidence = "";
+                  _names = "";
+                });
+              },
+              child: Text(
+                "Clear",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ],
         ),
       ),
